6c883b09b295b13483d8379977290ce4688bfa2c
Drew Town <drew.town+github@gmail.com>
2024-04-05 13:58:39 -0600

    Add more pages

---
+++ components/TeamList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-screen-xl px-4 py-12 text-center sm:px-6 lg:px-8">
+      <div class="space-y-8 sm:space-y-12">
+        <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+          <h2 class="text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl">
+            Get to Know Our Team
+          </h2>
+          <p class="text-lg leading-7 text-gray-500">
+            Meet the dedicated team of Friends of Ute Valley Park, who work hard to preserve the natural beauty of this cherished community resource and make it enjoyable for everyone.
+          </p>
+        </div>
+        <ul class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12">
+          <li v-for="teamMember in teamMembers"
+              :key="teamMember.name">
+            <div class="space-y-4">
+              <NuxtPicture v-if="teamMember.image !== ''"
+                           :img-attrs="{class: 'mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24'}"
+                           :src="teamMember.image"
+                           sizes="60px md:120px"
+                           alt="" />
+              <Icon v-else
+                    name="heroicons:user-circle"
+                    class="mx-auto h-20 w-20 text-green-800 lg:h-24 lg:w-24" />
+
+              <div class="space-y-2">
+                <div class="text-xs font-medium leading-4 lg:text-sm lg:leading-5">
+                  <h4>{{ teamMember.name }}</h4>
+                  <p class="text-green-700">
+                    {{ teamMember.title }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const teamMembers = [
+  { name: 'Drew Town', title: 'Chair', image: 'img/members/drew-town.png' },
+  { name: 'Christine Thomas', title: 'Vice-Chair', image: 'img/members/christine-thomas.jpg' },
+  { name: 'Megan Town', title: 'Treasurer', image: 'img/members/megan-town.jpg' },
+  { name: 'Molly Murrow', title: 'Secretary', image: 'img/members/molly-murrow.jpg' },
+  { name: 'Phill Emmert', title: 'Communications', image: 'img/members/phill-emmert.jpg' },
+  { name: 'Frank Self', title: 'Committee Member', image: 'img/members/frank-self.jpg' },
+];
+</script>
---
+++ pages/rules.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="relative mt-16 px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto mb-6 max-w-prose text-lg">
+      <h1 class="mb-8 mt-2 text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+        City of Colorado Springs Park <span class="text-green-700">Rules</span>
+      </h1>
+      <p class="text-2xl font-semibold text-gray-700">
+        The city of
+        <a href="https://coloradosprings.gov/parks/page/park-hours-and-rules?mlid=3191"
+           class="font-semibold text-green-600 no-underline"> Colorado Springs Parks, Recreation, and Cultural Services</a>
+        is responsible for creating and enforcing rules across all city parks.
+      </p>
+      <div class="mt-12 border-b border-gray-200 pb-5">
+        <h3 class="text-2xl font-bold leading-6 text-gray-900">
+          Park Hours
+        </h3>
+      </div>
+      <ul class="list-disc pl-6 marker:text-gray-300">
+        <li class="py-1">
+          November 1 to April 30: <span class="font-bold">5 am - 9 pm</span>.
+        </li>
+        <li class="py-1">
+          May 1 to October 31: <span class="font-bold">5 am - 10 pm</span>.
+        </li>
+      </ul>
+
+      <div class="mt-10 border-b border-gray-200 pb-5">
+        <h3 class="text-2xl font-bold leading-6 text-gray-900">
+          Ute Valley Park Exceptions
+        </h3>
+      </div>
+
+      <ol class="list-decimal pl-6">
+        <li class="py-1 pl-2">
+          There is <span class="font-bold">no</span> designated smoking area in Ute Valley Park.
+        </li>
+        <li class="py-1 pl-2">
+          There is <span class="font-bold">no</span> designated area where the pet waste rule does not apply in Ute Valley Park.
+        </li>
+        <li class="py-1 pl-2">
+          There is <span class="font-bold">no</span>&nbsp<a class="text-green-600 underline"
+                                                            href="/leavenotrace/dogs">dog park</a> or off-leash area in Ute Valley Park.
+        </li>
+      </ol>
+
+      <div class="mt-10 border-b border-gray-200 pb-5">
+        <h3 class="text-2xl font-bold leading-6 text-gray-900">
+          Park Rules
+        </h3>
+      </div>
+
+      <ol class="list-decimal pl-6">
+        <li class="py-1 pl-2">
+          Alcoholic beverages are prohibited in City Parks by Ordinance No. 9-9-410.
+        </li>
+        <li class="py-1 pl-2">
+          Parking/Vehicular Access: Motorized vehicles of all types including golf carts, gators, ATVs, etc, are strictly prohibited on park property, lawns, turf, restricted roadways,
+          bicycle/pedestrian pathways, creek pathways, turf or athletic fields. All cars must be parked in designated parking areas.
+        </li>
+        <li class="py-1 pl-2">
+          Trash and debris must be disposed of properly.
+        </li>
+        <li class="py-1 pl-2">
+          Open fires and charcoal grills are prohibited. Place propane grills on hard surfaces such as a concrete pad or picnic table.
+        </li>
+        <li class="py-1 pl-2">
+          It is unlawful for any person to injure, deface, destroy, sever or remove any park property.
+        </li>
+        <li class="py-1 pl-2">
+          If you are using amplified equipment for your picnic or event, you will need to obtain a noise permit through the Police Department per Ordinances No. 9-8-101 through 9-8-109. Please contact
+          CSPD at 444-7704 regarding noise permits. No permit is confirmed until the noise permit has been approved.
+        </li>
+        <li class="py-1 pl-2">
+          The posting of signs leading to a reservation area or hanging decorations or other materials on park buildings, structures or vegetation is prohibited.
+        </li>
+        <li class="py-1 pl-2">
+          The smoking of any cigarette, pipe, cigar or electronic smoking device is prohibited on park property unless a designated area is provided. No. 6-6-202
+        </li>
+        <li class="py-1 pl-2">
+          It is unlawful for any unauthorized person to attach any rope, wire or other contrivance to any tree or plant in any park.
+        </li>
+      </ol>
+
+      <div class="mt-12 border-b border-gray-200 pb-5">
+        <h3 class="text-2xl font-bold leading-6 text-gray-900">
+          Pet-related Rules
+        </h3>
+      </div>
+
+      <p class="my-2 font-bold">
+        9.9.301: ANIMAL DEFECATION:
+      </p>
+      <p>
+        It is unlawful for any person to allow any animal over which the person has control to defecate upon any park land without the excrement being removed by the person in control of the animal
+        from the park and dispose of properly. The Park Manager may designate areas of the parks where the rule does not apply. (Ord. 18-24)
+      </p>
+      <p class="my-2 font-bold">
+        9.9.302: DOGS, DOMESTIC ANIMALS AT LARGE:
+      </p>
+      <p>
+        A. It is unlawful for any person to lead or allow to be loose any animal upon park premises; dogs, however, may be led or carried but not allowed loose. Dogs may be loose but under the control
+        of the owner or keeper in those areas designated as dog parks or off-leash areas. Nothing in this section shall be construed as permitting the running of dogs at large. All dogs in those areas
+        where the animals are permitted without leashes shall be under control at all times.
+      </p>
+      <p>B. In any dog park or off-leash area designated by the Park Manager in which dogs are permitted without leashes, it is unlawful for any person:</p>
+      <ol class="mt-2 list-decimal pl-6">
+        <li class="py-1 pl-2">
+          To intentionally, knowingly or recklessly lead, bring or release any other type or species of animal into or upon the confined or fenced dog park or off-leash area; or
+        </li>
+        <li class="py-1 pl-2">
+          To fail to obey any posted rules, regulations or guidelines for use of the fenced dog park or off-leash area.
+        </li>
+        <li class="py-1 pl-2">
+          To bring a dangerous animal, as defined in section <a class="text-green-600 underline"
+                                                                href="https://codelibrary.amlegal.com/codes/coloradospringsco/latest/coloradosprings_co/0-0-0-7038">6.7.107
+          </a> (link is external) of this Code, into a fenced dog park or off-leash area. (Ord. 18-24)
+        </li>
+      </ol>
+
+      <div class="mt-12 border-b border-gray-200 pb-5">
+        <h3 class="text-2xl font-bold leading-6 text-gray-900">
+          Trail Etiquette
+        </h3>
+      </div>
+
+      <NuxtPicture src="img/rules/trail-right-of-way.png"
+                   width="600"
+                   height="600"
+                   sizes="300px md:600px"
+                   alt="Trail right of way sign" />
+      <ul class="list-disc pl-6 marker:text-gray-300">
+        <li class="py-1 pl-2">
+          Please, stay on the trail.
+        </li>
+        <li class="py-1 pl-2">
+          Share the trail. Ride, walk or run on the right, pass on the left.
+        </li>
+        <li class="py-1 pl-2">
+          Bicyclist - slow down when approaching others or curves. Warn others when passing.
+        </li>
+        <li class="py-1 pl-2">
+          Dogs must be on leash. Owner is responsible for cleaning up pet excrement.
+        </li>
+      </ul>
+    </div>
+  </div>
+  <LeaveNoTrace />
+</template>
