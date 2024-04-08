7247155d28f601c00b465c42944b322b369aaeaa
Drew Town <drew.town+github@gmail.com>
2024-04-08 13:28:15 -0600

    update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
 import type { Config } from 'tailwindcss'
 
 // Default are on https://tailwindcss.nuxtjs.org/tailwind/config#default-configuration
+const colors = require('tailwindcss/colors')
+
 export default <Partial<Config>>{
-  theme: {},
-  plugins: [],
-  content: [
-    './components/**/*.{vue,js,ts}',
-    './layouts/**/*.{vue,js,ts}',
-    './pages/**/*.{vue,js,ts}',
-    './composables/**/*.{vue,js,ts}',
-    './plugins/**/*.{vue,js,ts}',
-  ]
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      slate: colors.slate,
+      green: colors.green,
+      red: colors.red,
+      teal: colors.teal,
+    },
+  },
 }
